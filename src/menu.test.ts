47665e10c8a0db2184a95fe79a28e3b654194794
Dannel Albert <cuebix@gmail.com>
2022-06-21 22:16:58 -0400

    test: optionSelection with object options

--- src/menu.test.ts
+++ src/menu.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import makeMenuStore from './makeMenuStore'
+import type { MenuState } from './makeMenuStore'
 
 describe('menu', () => {
   describe('initialization', () => {
@@ -76,6 +77,56 @@ describe('menu', () => {
 
       expect(menu.options).toBe(options)
       expect(menu.selection).toBe(b)
+    })
+
+    it('should accept object options and mapped selection for single selection', () => {
+      const a = {
+        id: 1,
+        label: 'A'
+      }
+      const b = {
+        id: 2,
+        label: 'B'
+      }
+      const c = {
+        id: 3,
+        label: 'C'
+      }
+      const options = [a, b, c]
+      const optionSelectionHandler = (menu: MenuState<typeof a, typeof a.id>) => {
+        return menu.options.filter((o) => o.id === menu.selection)[0]
+      }
+
+      const [menu, _menuApi] = makeMenuStore({ options, selection: b.id }, { optionSelectionHandler })
+
+      expect(menu.options).toBe(options)
+      expect(menu.selection).toBe(b.id)
+      expect(menu.optionSelection).toBe(b)
+    })
+
+    it('should accept object options and mapped selections for multiple selections', () => {
+      const a = {
+        id: 1,
+        label: 'A'
+      }
+      const b = {
+        id: 2,
+        label: 'B'
+      }
+      const c = {
+        id: 3,
+        label: 'C'
+      }
+      const options = [a, b, c]
+      const optionSelectionHandler = (menu: MenuState<typeof a, number[]>) => {
+        return menu.options.filter((o) => menu.selection?.includes(o.id))
+      }
+
+      const [menu, _menuApi] = makeMenuStore({ options, selection: [a.id, c.id] }, { optionSelectionHandler })
+
+      expect(menu.options).toBe(options)
+      expect(menu.selection).toEqual([a.id, c.id])
+      expect(menu.optionSelection).toEqual([a, c])
     })
   })
 
